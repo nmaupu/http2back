620c1ad6d120f4886cccefc9e48cab72d683160b
nmaupu <nmaupu@gmail.com>
2017-10-08 01:48:41 +0200

    Json response + func pointer to get provider
    - all response are now json encoded
    - passing a function pointer to server.Start to get needed provider

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"github.com/nmaupu/http2back/server"
 )
 
+func getProviderFilesystem() server.Provider {
+	return server.Filesystem{"/tmp/test"}
+}
+
 func main() {
 	addr := "127.0.0.1"
 	port := 8080
-	provider := server.Filesystem{"/tmp/test"}
-	server.Start(&port, &addr, provider)
+
+	server.Start(&port, &addr, getProviderFilesystem)
 }
--- server/http.go
+++ server/http.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"math/rand"
@@ -9,15 +10,23 @@ import (
 )
 
 // Globals
-var provider Provider = nil
+var getProv func() Provider = nil
 
 // HTTP API
 func handleRequest(w http.ResponseWriter, r *http.Request) {
+	type JsonResult struct {
+		Result string `json:"result"`
+	}
+	type JsonError struct {
+		Error string `json:"error"`
+	}
 	defer func() {
 		if r := recover(); r != nil {
-			log.Println(fmt.Sprintf("Error : %s", r))
+			je := JsonError{fmt.Sprint(r)}
+			log.Println(je.Error)
+			j, _ := json.Marshal(je)
 			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(fmt.Sprintf("500 - %s", r)))
+			w.Write(j)
 		}
 	}()
 
@@ -27,7 +36,7 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == "PUT" || r.Method == "POST" {
-		r.ParseMultipartForm(32 << 20) // 32768
+		r.ParseMultipartForm(32 << 20)
 		in, handler, err := r.FormFile("file")
 		defer in.Close()
 		if err != nil {
@@ -37,20 +46,24 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		// Unique filename
 		t := time.Now()
 		name := fmt.Sprintf("%s-%s.%06d", handler.Filename, t.Format("20060102_150405"), rand.Intn(100000))
-		ret := provider.Copy(in, name)
+		ret := getProv().Copy(in, name)
 
-		defer log.Printf("Called : %s %s - file %s -> %s", r.Method, r.URL.Path, handler.Filename, ret)
-		defer w.Write([]byte(fmt.Sprintf("Saved ok - %s", ret)))
+		// Send result
+		log.Printf("Called : %s %s - file %s -> %s", r.Method, r.URL.Path, handler.Filename, ret)
+		jr := JsonResult{ret}
+		j, _ := json.Marshal(jr)
+		w.Write(j)
+
 	} else {
 		panic(fmt.Sprintf("%s is an unsupported method for %s", r.Method, r.URL.Path))
 	}
 }
 
 // Starting the HTTP server
-func Start(port *int, bind_addr *string, p Provider) {
-	provider = p
+func Start(port *int, bind_addr *string, getProvider func() Provider) {
+	getProv = getProvider
 
-	log.Printf("Starting http server on %s:%d using provider %s", *bind_addr, *port, p)
+	log.Printf("Starting http server on %s:%d using provider %s", *bind_addr, *port, getProv())
 
 	http.HandleFunc("/", handleRequest)
 
