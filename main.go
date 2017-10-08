9e2b1b81fd77712aceb6b0f1b506b55597b7f8df
nmaupu <nmaupu@gmail.com>
2017-10-08 18:36:03 +0200

    Adding opts and config file management
    - opts is very minimalist and uses https://github.com/jawher/mow.cli
    - config file management is using https://github.com/spf13/viper and
      will be unriched later on

--- main.go
+++ main.go
@@ -1,16 +1,69 @@
 package main
 
 import (
+	"fmt"
+	"github.com/jawher/mow.cli"
 	"github.com/nmaupu/http2back/server"
+	"github.com/spf13/viper"
+	"log"
+	"os"
 )
 
-func getProviderFilesystem() server.Provider {
-	return server.Filesystem{"/tmp/test"}
+const (
+	appName             = "http2back"
+	defaultProviderDest = "/tmp"
+)
+
+func getDefaultProvider() server.Provider {
+	return server.Filesystem{defaultProviderDest}
 }
 
 func main() {
-	addr := "127.0.0.1"
-	port := 8080
+	app := cli.App(appName, "")
+	app.Spec = "[--bind=<address>] [--port=<port>]"
 
-	server.Start(&port, &addr, getProviderFilesystem)
+	var (
+		addr         = app.StringOpt("b bind", "127.0.0.1", "Bind address")
+		port         = app.IntOpt("p port", 8080, "Port to listen from")
+		providerFunc = getDefaultProvider
+	)
+
+	app.Action = func() {
+		var (
+			vAddr, vProvider, vDest string
+			vPort                   int
+		)
+		viper.SetConfigName(appName)
+		viper.AddConfigPath(fmt.Sprintf("/etc/%s/", appName))
+		viper.AddConfigPath(fmt.Sprintf("$HOME/.%s", appName))
+		viper.AddConfigPath(".")
+		err := viper.ReadInConfig()
+		if err != nil {
+			log.Printf("%s, using defaults\n", err)
+		} else {
+			vAddr = viper.GetString("bind_address")
+			vPort = viper.GetInt("port")
+			vProvider = viper.GetString("provider.name")
+
+			if vAddr != "" && *addr == "127.0.0.1" {
+				*addr = vAddr
+			}
+			if vPort > 0 && *port == 8080 {
+				*port = vPort
+			}
+
+			switch vProvider {
+			case "filesystem":
+				vDest = viper.GetString("provider.dest")
+				providerFunc = func() server.Provider { return server.Filesystem{vDest} }
+			default:
+				vProvider = "filesystem"
+				vDest = "/tmp"
+			}
+		}
+
+		server.Start(port, addr, providerFunc)
+	}
+
+	app.Run(os.Args)
 }
